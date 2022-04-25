f4ddff9632046244ba5996afaf40fcc5210e6b7a
Anthony Badea <badeaa3@gmail.com>
2022-04-25 14:58:58 +0200

    Improved checkpointing, update evaluation, move callbacks from make model to in the train script, add checkpoint folder to gitignore.

--- ML_Keras/evaluate.py
+++ ML_Keras/evaluate.py
@@ -1,41 +1,81 @@
+'''
+Author: Anthony Badea
+Date: Monday April 25, 2022
+'''
+
 # python imports
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
+import json
+import tensorflow as tf
 
 # custom code
 from get_data import get_full_data
 from make_model import make_model
 
-def evaluate(model, x, y, normweight):
+# Tensorflow GPU settings
+physical_devices = tf.config.list_physical_devices('GPU') 
+if physical_devices:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
-	# prepare data
-	x = x.reshape(x.size, 1)
-	y = y.reshape(x.size, 1)
-	normweight = normweight.reshape(x.size, 1)
-	xa = x[y==0]
-	xb = x[y==1]
-	bins = np.linspace(0, 8000, 100)
-	normweightsa = normweight[y==0]
-	normweightsb = normweight[y==1]
+def main():
 
-	# make model prediction
-	p = model.predict(x)
+    # user options
+    ops = options()
+    if ops.conf:
+        with open(ops.conf) as f:
+            print(f"opening {ops.conf}")
+            conf = json.load(f)
+    else:
+        conf = {
+            "file": ops.inFile,
+            "ndense" : ops.ndense,
+            "nnode_per_dense" : ops.nnode_per_dense
+        }
 
-	# plot
-	xa = np.multiply(xa, 1000)
-	xb = np.multiply(xb, 1000)
-	c0, _, _ = plt.hist(xa, bins = bins, alpha = 0.5, weights = normweightsa, label = '#QuarkJets > 0', color = 'red', density=True)
-	c1, _, _ = plt.hist(xb, bins = bins, alpha = 0.5, weights = normweightsb, label = '#QuarkJets = 0', color = 'blue', density=True)
-	p = np.array(p).reshape(x.size, 1)
-	_pp = p[y==0]
-	final_weights = (1-_pp)/_pp
-	final_weights *= normweightsa
-	c2, _, _ = plt.hist(xa, bins = bins, alpha = 0.5, weights = final_weights, label = '#Quarks > 0 reweighted to #QuarksJets = 0', color = 'yellow', density=True) 
-	plt.legend()
-	plt.show()
+    # load model
+    model = make_model(input_dim=1, ndense=conf["ndense"], nnode_per_dense=conf["nnode_per_dense"], learning_rate=1e-3)
+    model.summary()
+    model.load_weights(ops.model_weights)
+    x, y, normweight = get_full_data(conf["file"])
+
+    # prepare data
+    x = x.reshape(x.size, 1)
+    y = y.reshape(x.size, 1)
+    normweight = normweight.reshape(x.size, 1)
+    xa = x[y==0]
+    xb = x[y==1]
+    bins = np.linspace(0, 8000, 100)
+    normweightsa = normweight[y==0]
+    normweightsb = normweight[y==1]
+
+    # make model prediction
+    p = model.predict(x)
+
+    # plot
+    xa = np.multiply(xa, 1000)
+    xb = np.multiply(xb, 1000)
+    c0, _, _ = plt.hist(xa, bins = bins, alpha = 0.5, weights = normweightsa, label = '#QuarkJets > 0', color = 'red', density=True)
+    c1, _, _ = plt.hist(xb, bins = bins, alpha = 0.5, weights = normweightsb, label = '#QuarkJets = 0', color = 'blue', density=True)
+    p = np.array(p).reshape(x.size, 1)
+    _pp = p[y==0]
+    final_weights = (1-_pp)/_pp
+    final_weights *= normweightsa
+    c2, _, _ = plt.hist(xa, bins = bins, alpha = 0.5, weights = final_weights, label = '#Quarks > 0 reweighted to #QuarksJets = 0', color = 'yellow', density=True) 
+    plt.legend()
+    plt.show()
+
+def options():
+    parser = argparse.ArgumentParser()
+    # input files d
+    parser.add_argument("-c",  "--conf", help="Configuration file. If provided, all other settings are overruled.", default=None)
+    parser.add_argument("-i",  "--inFile", help="Input file.", default=None)
+    parser.add_argument("-m",  "--model_weights", help="Model weights.", default=None)
+    # model settings
+    parser.add_argument("-nl", "--ndense", help="Number of dense layers.", default=1, type=int)
+    parser.add_argument("-nd", "--nnode_per_dense", help="Number of nodes per dense layer.", default=30, type=int)
+    return parser.parse_args()
 
 if __name__ == "__main__":
-	model, callbacks = make_model(input_dim=1, nodes=30, learning_rate=1e-3)
-	model.load_weights("best_model.h5")
-	Xs, ys, wgts = get_full_data('mc16a_dijets_JZAll_for_reweighting.h5')
-	evaluate(model, Xs, ys, wgts)
+    main()
--- ML_Keras/train.py
+++ ML_Keras/train.py
@@ -1,6 +1,15 @@
+'''
+Author: Anthony Badea
+Date: Monday April 25, 2022
+'''
+
 # python imports
 import h5py
 import tensorflow as tf
+import argparse
+import json
+import datetime
+import os
 
 # custom imports
 from make_model import make_model
@@ -8,21 +17,33 @@ from get_data import get_data
 
 # Tensorflow GPU settings
 physical_devices = tf.config.list_physical_devices('GPU') 
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+if physical_devices:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 def main():
 
+    # user options
+    ops = options()
+    if ops.conf:
+        with open(ops.conf) as f:
+            print(f"opening {ops.conf}")
+            conf = json.load(f)
+    else:
+        conf = {
+            "file": ops.inFile,
+            "nepochs": ops.nepochs,
+            "train_batch_size": ops.train_batch_size,
+            "val_batch_size": ops.val_batch_size,
+            "validation_steps" : ops.validation_steps,
+            "learning_rate" : ops.learning_rate,
+            "input_dim" : 1,
+            "ndense" : ops.ndense,
+            "nnode_per_dense" : ops.nnode_per_dense
+        }
+        with open('conf.json', 'w') as fp:
+            json.dump(conf, fp)
+
     # training configuration
-    conf = {
-        "file": "mc16a_dijets_JZAll_for_reweighting.h5",
-        "nepochs": 5,
-        "train_batch_size": 2048,
-        "val_batch_size": 256,
-        "validation_steps" : 10,
-        "learning_rate" : 1e-3,
-        "input_dim" : 1,
-        "nodes" : 30,
-    }
     with h5py.File(conf["file"]) as f:
         conf["num_samples"] = f["data"]["ZeroQuarkJetsFlag"].shape[0]
     conf["train_steps_per_epoch"] = conf["num_samples"] // conf["train_batch_size"]
@@ -33,8 +54,26 @@ def main():
     val_data_gen = get_data(conf["file"], conf["nepochs"], conf["val_batch_size"]) # to-do be more careful about partitioning the train and val data
 
     # make model
-    model, callbacks = make_model(input_dim=1, nodes=30, learning_rate=conf["learning_rate"])
+    model = make_model(input_dim=1, ndense=conf["ndense"], nnode_per_dense=conf["nnode_per_dense"], learning_rate=conf["learning_rate"])
     model.summary()
+
+    # make callbacks
+    callbacks = []
+    # EarlyStopping
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        patience=10, mode="min", restore_best_weights=True, monitor="val_loss"
+    )
+    callbacks.append(early_stopping)
+    # ModelCheckpoint
+    checkpoint_filepath = f'./checkpoints/{datetime.datetime.now().strftime("%H.%M.%S")}'
+    if not os.path.isdir(checkpoint_filepath):
+        os.makedirs(checkpoint_filepath)
+    model_checkpoint = tf.keras.callbacks.ModelCheckpoint(
+        checkpoint_filepath, monitor="val_loss", mode="min", save_best_only=True
+    )
+    callbacks.append(model_checkpoint)
+    # Terminate on NaN such that it is easier to debug
+    callbacks.append(tf.keras.callbacks.TerminateOnNaN())
 
     # train
     model.fit(
@@ -44,9 +83,25 @@ def main():
         callbacks=callbacks,
         verbose=1,
         validation_data=val_data_gen,
-        validation_steps=1
+        validation_steps=conf["validation_steps"]
     )
 
+
+def options():
+    parser = argparse.ArgumentParser()
+    # input files
+    parser.add_argument("-c",  "--conf", help="Configuration file. If provided, all other settings are overruled.", default=None)
+    parser.add_argument("-i",  "--inFile", help="Input file.", default=None)
+    # train settings
+    parser.add_argument("-ne", "--nepochs", help="Number of epochs.", default=1, type=int)
+    parser.add_argument("-tb", "--train_batch_size", help="Training batch size.", default=2048, type=int)
+    parser.add_argument("-vb", "--val_batch_size", help="Validation batch size.", default=256, type=int)
+    parser.add_argument("-vs", "--validation_steps", help="Number of validation steps.", default=1, type=int)
+    parser.add_argument("-lr", "--learning_rate", help="Learning rate", default=1e-3, type=float)
+    # model settings
+    parser.add_argument("-nl", "--ndense", help="Number of dense layers.", default=1, type=int)
+    parser.add_argument("-nd", "--nnode_per_dense", help="Number of nodes per dense layer.", default=30, type=int)
+    return parser.parse_args()
 
 if __name__ == "__main__":
     main()
