Make the model in a loop with units (nodes), input_dim, and activations set beforehand in lists. This ensures that the correct configurations are used. Noteably the input variable ndense now exactly corresponds to the number of layers rather than the number of hidden layers, i.e. specifying ndense=1 will produce a single layer network with output dim of 1 and ndense=2 will have two layers first with a 30 node layer.

ML_Keras/make_model.py
@@ -10,9 +10,12 @@ def make_model(**kargs):
     
     # Create model
     model = tf.keras.Sequential()
-    for i in range(kargs["ndense"]):
-        model.add(tf.keras.layers.Dense(kargs["nnode_per_dense"], input_dim=kargs["input_dim"], activation="relu"))
-    model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
+    
+    nodes = [kargs["nnode_per_dense"] for i in range(kargs["ndense"]-1)] + [1]
+    input_dims = [kargs["input_dim"]] + nodes
+    activations = ["relu" for i in range(kargs["ndense"]-1)] + ["sigmoid"]
+    for node, input_dim, activation in zip(nodes, input_dims, activations):
+        model.add(tf.keras.layers.Dense(node, input_dim=input_dim, activation=activation))
     
     # Compile model
     model.compile(