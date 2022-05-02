6bcf424d25bef4cee11dfcc0cf9144e2284dd021
jbossios <bossjonad@gmail.com>
2022-05-02 15:10:06 +0200

    Add setup.py

---
+++ ML_Keras/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+    name="jbossios",
+
+    version="1.0.0",
+
+    author="Jonathan Bossio",
+
+    author_email="",
+
+    description="Background estimation using reweighting for the SUSY RPV MJ search",
+
+    long_description=long_description,
+
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/jbossios/ATLAS_SUSY_RPVMJ_Reweighting",
+
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+
+        "Programming Language :: Python :: 3",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Operating System :: OS Independent",
+
+    ],
+
+    python_requires='>=3.8',
+
+)
