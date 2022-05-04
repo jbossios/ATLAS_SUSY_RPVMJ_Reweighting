378e2dde41b6c90c315200839d0f4cf345f5cc34
jbossios <bossjonad@gmail.com>
2022-05-04 15:47:37 +0200

    Fix spacing

--- ML_Keras/train.py
+++ ML_Keras/train.py
@@ -83,7 +83,7 @@ def main(config = None):
     # set seeds to get reproducible results (only if requested)
     if seed is not None:
         try:
-	    tf.random.set_seed(seed)
+            tf.random.set_seed(seed)
         except:  # deprecated in newer tf versions
             tf.keras.utils.set_random_seed(seed)
         python_random.seed(seed)
