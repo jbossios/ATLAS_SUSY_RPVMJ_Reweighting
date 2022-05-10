280c1ac2135185c6cce4cdc599c68ed68d48df92
Anthony Badea <anthony.badea@cern.ch>
2022-05-10 16:58:57 +0200

    testing

--- ML_Keras/WeightedSamplingDataLoader.py
+++ ML_Keras/WeightedSamplingDataLoader.py
@@ -1,21 +1,27 @@
 import numpy as np
 import h5py
 import tensorflow as tf
+import time
 
 class WeightedSamplingDataLoader(tf.data.Dataset):
 
     def _generator(probabilities, fileidx, fileList, num_batches, batch_size):
         for iB in range(num_batches):
+            start = time.time()
             idx = np.random.choice(range(0,len(probabilities)), size=batch_size, p = probabilities, replace = True)
+            print ("Time elapsed for idx:", time.time() - start)
             print(idx)
+            start = time.time()
             samples = fileidx[idx]
             samples = samples[samples[:, 0].argsort()]
             # get unique files and list of events per file
             files = np.unique(samples[:,0])
             events = [samples[np.where(samples[:,0] == i)][:,1] for i in files]
+            print ("Time elapsed middle:", time.time() - start)
             # create batch
             x = []
             y = []
+            start = time.time()
             for iF,iE in zip(files,events):
                 with h5py.File(fileList[iF], "r") as hf:
                     # pick up the kinematics
@@ -28,6 +34,7 @@ class WeightedSamplingDataLoader(tf.data.Dataset):
                     y.append(np.zeros((j[iE].shape[0])))      
             x = np.concatenate(x)
             y = np.concatenate(y)
+            print ("Time elapsed for getting data from files:", time.time() - start)
             yield x, y
 
     def __new__(self, probabilities, fileidx, fileList, num_batches, batch_size):
@@ -40,12 +47,12 @@ class WeightedSamplingDataLoader(tf.data.Dataset):
             args=(probabilities, fileidx, fileList, num_batches, batch_size,)
         )
 
-# FileList = "/eos/atlas/atlascerngroupdisk/phys-susy/RPV_mutlijets_ANA-SUSY-2019-24/abadea/ParticleNet/v1/Dijets/list.txt"
-# fileList = sorted([line.strip() for line in open(FileList,"r")])
-fileList = [
-    "/Users/anthonybadea/Documents/ATLAS/rpvmj/ParticleNet/tf-keras/user.jbossios.364708.e7142_e5984_s3126_r10724_r10726_p4355.27261077._000017.trees_expanded_spanet.h5",
-    "/Users/anthonybadea/Documents/ATLAS/rpvmj/ParticleNet/tf-keras/user.jbossios.364712.e7142_e5984_s3126_r10724_r10726_p4355.27261089._000001.trees_expanded_spanet.h5"
-]
+FileList = "/eos/atlas/atlascerngroupdisk/phys-susy/RPV_mutlijets_ANA-SUSY-2019-24/abadea/ParticleNet/v1/Dijets/list.txt"
+fileList = sorted([line.strip() for line in open(FileList,"r")])
+# fileList = [
+#     "/Users/anthonybadea/Documents/ATLAS/rpvmj/ParticleNet/tf-keras/user.jbossios.364708.e7142_e5984_s3126_r10724_r10726_p4355.27261077._000017.trees_expanded_spanet.h5",
+#     "/Users/anthonybadea/Documents/ATLAS/rpvmj/ParticleNet/tf-keras/user.jbossios.364712.e7142_e5984_s3126_r10724_r10726_p4355.27261089._000001.trees_expanded_spanet.h5"
+# ]
 loadWeightSampler = True
 if loadWeightSampler:
     x = np.load("WeightSamplerDijets.npz",allow_pickle=True)
@@ -69,9 +76,9 @@ else:
 probabilities = weights / weights.sum()
 
 num_batches = 5
-batch_size = 5
+batch_size = 1024
 
-dataset = WeightedSamplingDataLoader(probabilities, fileidx, fileList, num_batches, batch_size)
+dataset = WeightedSamplingDataLoader(probabilities, fileidx, fileList, num_batches, batch_size).prefetch(tf.data.AUTOTUNE)
 
 num_epochs = 1
 for epoch_num in range(num_epochs):
