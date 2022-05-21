2352298748e8751189f49ebbf7bd2fe886690649
Anthony Badea <badeaa3@gmail.com>
2022-05-21 23:57:56 +0200

    Update weighted sampler

--- CreateH5WeightedSample.py
+++ CreateH5WeightedSample.py
@@ -1,7 +1,14 @@
+'''
+Author: Anthony Badea
+Date: May 21, 2022
+Purpose: Sample dijet files based on event weights to create training files
+'''
+
 import numpy as np
 import h5py
 import argparse
 import os
+from multiprocessing import Pool
 
 def main():
 
@@ -29,15 +36,29 @@ def main():
 		np.savez("WeightSamplerDijets.npz",**{"weights":weights,"fileidx":fileidx})
 
 	probabilities = weights / weights.sum()
-	n_files = 1
 
-	n_samples = 100
-	sample(probabilities, fileidx, fileList, n_samples, "test.npz")
 
-def sample(probabilities, fileidx, fileList, n_samples, outName):
+	n_files = int(ops.n_total_events/ops.n_events_per_file)
+	configs = []
+	for iF in range(n_files):
+		configs.append({
+			"probabilities" : probabilities, 
+			"fileidx" : fileidx,
+			"fileList" : fileList,
+			"n_samples" : ops.n_events_per_file,
+			"outName" : os.path.join(ops.outDir, f"Dijets_Sampled_v{ops.version}_{iF}.npz")
+		})
+	
+	if ops.ncpu > 1:
+		results = Pool(ncpu).map(add_normweight, configs)
+	else:
+		for conf in configs:
+			sample(conf)
+
+def sample(conf):
     # get sample list
-    idx = np.random.choice(range(0,len(probabilities)), size=n_samples, p = probabilities, replace = True)
-    samples = fileidx[idx]
+    idx = np.random.choice(range(0,len(conf["probabilities"])), size=conf["n_samples"], p = conf["probabilities"], replace = True)
+    samples = conf["fileidx"][idx]
     samples = samples[samples[:, 0].argsort()]
     # get unique files and list of events per file
     files = np.unique(samples[:,0])
@@ -46,7 +67,7 @@ def sample(probabilities, fileidx, fileList, n_samples, outName):
     x = []
     y = []
     for iF,iE in zip(files,events):
-        with h5py.File(fileList[iF], "r") as hf:
+        with h5py.File(conf["fileList"][iF], "r") as hf:
             # pick up the kinematics
             m = np.array(hf["source"]["mass"])
             pt = np.array(hf["source"]["pt"])
@@ -59,13 +80,17 @@ def sample(probabilities, fileidx, fileList, n_samples, outName):
     y = np.concatenate(y)
 
     # save in same format as spanet style
-    np.savez(outName, **{"x":x,"y":y})
+    np.savez(conf["outName"], **{"x":x,"y":y})
 
 def options():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i",  "--inFile", help="Input file.", default=None)
-    parser.add_argument("-o",  "--outDir", help="Output directory", default="./")
-    parser.add_argument("-w",  "--weightSampler", help="Already made file to sample weights from. If not provided then one will be made automatically.", default=None)
+    parser.add_argument("-i", "--inFile", help="Input file.", default=None)
+    parser.add_argument("-o", "--outDir", help="Output directory", default="./")
+    parser.add_argument("-w", "--weightSampler", help="Already made file to sample weights from. If not provided then one will be made automatically.", default=None)
+    parser.add_argument("-v", '--version', help="File version", default='')
+    parser.add_argument("-j", '--ncpu', help="Number of cores to use in multiprocessing", default=1, type=int)
+    parser.add_argument("-nt", '--n_total_events', help="Total number of events to sample.", default=100, type=int)
+    parser.add_argument("-nf", '--n_events_per_file', help="Number of events per file.", default=100, type=int)
     return parser.parse_args()
 
 def handleInput(data):
