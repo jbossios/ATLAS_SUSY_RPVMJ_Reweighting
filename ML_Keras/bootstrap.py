94294467e94293fbb5e00ef0c924b9701093749a
Anthony Badea <anthony.badea@cern.ch>
2022-06-07 23:09:49 +0200

    normalize before prediction. not sure if i need to normalize all together before predicting or if this is ok but for now will leave it

--- ML_Keras/bootstrap.py
+++ ML_Keras/bootstrap.py
@@ -201,13 +201,13 @@ def train(conf):
 
     # predict in each region
     logfile.write(f"Prediction Region A with {conf['RegA_x'].shape[0]} events" + "\n")
-    RegA_p = model.predict(conf["RegA_x"],batch_size=10000).flatten()
+    RegA_p = model.predict((conf["RegA_x"]-np.mean(conf["RegA_x"]))/np.std(conf["RegA_x"]),batch_size=10000).flatten()
     logfile.write(f"Prediction Region B with {conf['RegB_x'].shape[0]} events" + "\n")
-    RegB_p = model.predict(conf["RegB_x"],batch_size=10000).flatten()
+    RegB_p = model.predict((conf["RegB_x"]-np.mean(conf["RegB_x"]))/np.std(conf["RegB_x"]),batch_size=10000).flatten()
     logfile.write(f"Prediction Region C with {conf['RegC_x'].shape[0]} events" + "\n")
-    RegC_p = model.predict(conf["RegC_x"],batch_size=10000).flatten()
+    RegC_p = model.predict((conf["RegC_x"]-np.mean(conf["RegC_x"]))/np.std(conf["RegC_x"]),batch_size=10000).flatten()
     logfile.write(f"Prediction Region D with {conf['RegD_x'].shape[0]} events" + "\n")
-    RegD_p = model.predict(conf["RegD_x"],batch_size=10000).flatten()
+    RegD_p = model.predict((conf["RegD_x"]-np.mean(conf["RegD_x"]))/np.std(conf["RegD_x"]),batch_size=10000).flatten()
     np.savez(
         os.path.join(checkpoint_dir,"predictions.npz"),
         **{
