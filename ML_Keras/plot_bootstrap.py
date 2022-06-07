2f012a80ae5efc38761b60dbe0e674ab8b66dbff
Anthony Badea <badeaa3@gmail.com>
2022-06-07 23:10:05 +0200

    plot for many bootstraps

--- ML_Keras/plot_bootstrap.py
+++ ML_Keras/plot_bootstrap.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import argparse
 import os
 import gc
+from glob import glob
 
 # matplotlib
 plt.rcParams['figure.figsize'] = (4,4)
@@ -85,46 +86,50 @@ def main():
     gc.collect()
 
     # load predictions
-    with np.load(ops.predFile, allow_pickle=True) as preds:
-        # reweight
-        RegA_reweighted = RegA_weights * np.exp(preds["RegA_p"])
-        RegB_reweighted = RegB_weights * np.exp(preds["RegB_p"])
+    pred_files = handleInput(ops.predFile)
 
-    # Reweight A -> C
-    fig, [ax,rx] = plt.subplots(2,1,constrained_layout=False,sharey=False,sharex=True,gridspec_kw={"height_ratios": [3.5,1], 'hspace':0.0},)
-    rx.set_ylabel("Ratio\nTo RegC")
-    rx.set_xlabel(r"H$_{\mathrm{T}}$ [GeV]")
-    rx.set_ylim(0,2)
-    ax.set_ylabel("Density of Events")
-    ax.set_yscale("log")
-    bins = np.linspace(0, 13000, 100)
-    c0, bin_edges, _ = ax.hist(RegA_ht, bins = bins, weights = RegA_weights, label = rf'RegA NQuarkJets $<$ {cut_nQuarkJets}', color = colors[0], density=ops.density, histtype="step", lw=2)
-    c1, bin_edges, _ = ax.hist(RegC_ht, bins = bins, weights = RegC_weights, label = rf'RegC NQuarkJets $\geq$ {cut_nQuarkJets}', color = colors[1], density=ops.density, histtype="step", lw=2)
-    c2, bin_edges, _ = ax.hist(RegA_ht, bins = bins, weights = RegA_reweighted, label = rf'Reweight RegA $\rightarrow$ RegC', color = colors[2], density=ops.density, histtype="step", lw=2) 
-    rx.plot((bin_edges[:-1] + bin_edges[1:]) / 2, c0/(c1 + 10**-50), 'o-', label = rf'RegA $/$ RegC', color = colors[0], lw=1)
-    rx.plot((bin_edges[:-1] + bin_edges[1:]) / 2, c2/(c1 + 10**-50), 'o-', label = rf'Reweighted RegA $/$ RegC', color = colors[2], lw=1)
-    rx.plot((bin_edges[:-1] + bin_edges[1:]) / 2,[1] * len((bin_edges[:-1] + bin_edges[1:]) / 2), ls="--",color="black",alpha=0.8)
-    ax.legend(title=rf"minAvgMass $<$ {cut_minAvgMass} GeV", loc="best", prop={'size': 8}, framealpha=0.0)
-    # rx.legend(title="", loc="best", prop={'size': 7}, framealpha=0.0)
-    plt.savefig(os.path.join(ops.outDir,'reweightAtoC.pdf'), bbox_inches="tight")
+    for iP, pred_file in enumerate(pred_files):
 
-    # Reweight B -> D
-    fig, [ax,rx] = plt.subplots(2,1,constrained_layout=False,sharey=False,sharex=True,gridspec_kw={"height_ratios": [3.5,1], 'hspace':0.0},)
-    rx.set_ylabel("Ratio\nTo RegD")
-    rx.set_xlabel(r"H$_{\mathrm{T}}$ [GeV]")
-    rx.set_ylim(0,2)
-    ax.set_ylabel("Density of Events")
-    ax.set_yscale("log")
-    bins = np.linspace(0, 13000, 100)
-    c0, bin_edges, _ = ax.hist(RegB_ht, bins = bins, weights = RegB_weights, label = rf'RegB NQuarkJets $<$ {cut_nQuarkJets}', color = colors[0], density=ops.density, histtype="step", lw=2)
-    c1, bin_edges, _ = ax.hist(RegD_ht, bins = bins, weights = RegD_weights, label = rf'RegD NQuarkJets $\geq$ {cut_nQuarkJets}', color = colors[1], density=ops.density, histtype="step", lw=2)
-    c2, bin_edges, _ = ax.hist(RegB_ht, bins = bins, weights = RegB_reweighted, label = rf'Reweight RegB $\rightarrow$ RegD', color = colors[2], density=ops.density, histtype="step", lw=2) 
-    rx.plot((bin_edges[:-1] + bin_edges[1:]) / 2, c0/(c1 + 10**-50), 'o-', label = rf'RegB $/$ RegD', color = colors[0], lw=1)
-    rx.plot((bin_edges[:-1] + bin_edges[1:]) / 2, c2/(c1 + 10**-50), 'o-', label = rf'Reweighted RegB $/$ RegD', color = colors[2], lw=1)
-    rx.plot((bin_edges[:-1] + bin_edges[1:]) / 2,[1] * len((bin_edges[:-1] + bin_edges[1:]) / 2), ls="--",color="black",alpha=0.8)
-    ax.legend(title=rf"minAvgMass $\geq$ {cut_minAvgMass} GeV", loc="best", prop={'size': 8}, framealpha=0.0)
-    # rx.legend(title="", loc="best", prop={'size': 7}, framealpha=0.0)
-    plt.savefig(os.path.join(ops.outDir,'reweightBtoD.pdf'), bbox_inches="tight")
+        with np.load(pred_file, allow_pickle=True) as preds:
+            # reweight
+            RegA_reweighted = RegA_weights * np.exp(preds["RegA_p"])
+            RegB_reweighted = RegB_weights * np.exp(preds["RegB_p"])
+
+        # Reweight A -> C
+        fig, [ax,rx] = plt.subplots(2,1,constrained_layout=False,sharey=False,sharex=True,gridspec_kw={"height_ratios": [3.5,1], 'hspace':0.0},)
+        rx.set_ylabel("Ratio\nTo RegC")
+        rx.set_xlabel(r"H$_{\mathrm{T}}$ [GeV]")
+        rx.set_ylim(0,2)
+        ax.set_ylabel("Density of Events")
+        ax.set_yscale("log")
+        bins = np.linspace(0, 13000, 100)
+        c0, bin_edges, _ = ax.hist(RegA_ht, bins = bins, weights = RegA_weights, label = rf'RegA NQuarkJets $<$ {cut_nQuarkJets}', color = colors[0], density=ops.density, histtype="step", lw=2)
+        c1, bin_edges, _ = ax.hist(RegC_ht, bins = bins, weights = RegC_weights, label = rf'RegC NQuarkJets $\geq$ {cut_nQuarkJets}', color = colors[1], density=ops.density, histtype="step", lw=2)
+        c2, bin_edges, _ = ax.hist(RegA_ht, bins = bins, weights = RegA_reweighted, label = rf'Reweight RegA $\rightarrow$ RegC', color = colors[2], density=ops.density, histtype="step", lw=2) 
+        rx.plot((bin_edges[:-1] + bin_edges[1:]) / 2, c0/(c1 + 10**-50), 'o-', label = rf'RegA $/$ RegC', color = colors[0], lw=1)
+        rx.plot((bin_edges[:-1] + bin_edges[1:]) / 2, c2/(c1 + 10**-50), 'o-', label = rf'Reweighted RegA $/$ RegC', color = colors[2], lw=1)
+        rx.plot((bin_edges[:-1] + bin_edges[1:]) / 2,[1] * len((bin_edges[:-1] + bin_edges[1:]) / 2), ls="--",color="black",alpha=0.8)
+        ax.legend(title=rf"minAvgMass $<$ {cut_minAvgMass} GeV", loc="best", prop={'size': 8}, framealpha=0.0)
+        # rx.legend(title="", loc="best", prop={'size': 7}, framealpha=0.0)
+        plt.savefig(os.path.join(ops.outDir,f'reweightAtoC_bootstrap{iP}.pdf'), bbox_inches="tight")
+
+        # Reweight B -> D
+        fig, [ax,rx] = plt.subplots(2,1,constrained_layout=False,sharey=False,sharex=True,gridspec_kw={"height_ratios": [3.5,1], 'hspace':0.0},)
+        rx.set_ylabel("Ratio\nTo RegD")
+        rx.set_xlabel(r"H$_{\mathrm{T}}$ [GeV]")
+        rx.set_ylim(0,2)
+        ax.set_ylabel("Density of Events")
+        ax.set_yscale("log")
+        bins = np.linspace(0, 13000, 100)
+        c0, bin_edges, _ = ax.hist(RegB_ht, bins = bins, weights = RegB_weights, label = rf'RegB NQuarkJets $<$ {cut_nQuarkJets}', color = colors[0], density=ops.density, histtype="step", lw=2)
+        c1, bin_edges, _ = ax.hist(RegD_ht, bins = bins, weights = RegD_weights, label = rf'RegD NQuarkJets $\geq$ {cut_nQuarkJets}', color = colors[1], density=ops.density, histtype="step", lw=2)
+        c2, bin_edges, _ = ax.hist(RegB_ht, bins = bins, weights = RegB_reweighted, label = rf'Reweight RegB $\rightarrow$ RegD', color = colors[2], density=ops.density, histtype="step", lw=2) 
+        rx.plot((bin_edges[:-1] + bin_edges[1:]) / 2, c0/(c1 + 10**-50), 'o-', label = rf'RegB $/$ RegD', color = colors[0], lw=1)
+        rx.plot((bin_edges[:-1] + bin_edges[1:]) / 2, c2/(c1 + 10**-50), 'o-', label = rf'Reweighted RegB $/$ RegD', color = colors[2], lw=1)
+        rx.plot((bin_edges[:-1] + bin_edges[1:]) / 2,[1] * len((bin_edges[:-1] + bin_edges[1:]) / 2), ls="--",color="black",alpha=0.8)
+        ax.legend(title=rf"minAvgMass $\geq$ {cut_minAvgMass} GeV", loc="best", prop={'size': 8}, framealpha=0.0)
+        # rx.legend(title="", loc="best", prop={'size': 7}, framealpha=0.0)
+        plt.savefig(os.path.join(ops.outDir,f'reweightBtoD_bootstrap{iP}.pdf'), bbox_inches="tight")
 
 def options():
     parser = argparse.ArgumentParser()
@@ -136,6 +141,17 @@ def options():
     parser.add_argument("-m",  "--model_weights", help="Model weights.", default=None)
     parser.add_argument("-d", "--density", help="Make plots density=True", action="store_true")
     return parser.parse_args()
+
+def handleInput(data):
+    elif os.path.isfile(data) and ".npz" in os.path.basename(data):
+        return [data]
+    elif os.path.isfile(data) and ".txt" in os.path.basename(data):
+        return sorted([line.strip() for line in open(data,"r")])
+    elif os.path.isdir(data):
+        return sorted([os.path.join(data,i) for i in os.listdir(data)])
+    elif "*" in data:
+        return sorted(glob(data))
+    return []
 
 if __name__ == "__main__":
     main()
